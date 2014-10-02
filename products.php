5df264c2727590d3b1c52fe8cdb0c48521df73a9
Noxet <nooxet@gmail.com>
2014-10-03 00:42:45 +0200

    removed old products.php and replaces it with the new

--- products.php
+++ products.php
@@ -1,55 +1,69 @@
-<!DOCTYPE html>
+<?php
+require_once "connection.php";
+$_SESSION['total'] = 0;
+if(isset($_GET['action']) && $_GET['action'] == "add"){
+	$id = intval($_GET['id']);
+	if(isset($_SESSION['cart'][$id])){
+		$_SESSION['cart'][$id]['quantity']++;
+	}else{
+		$sql2 = "SELECT * FROM products WHERE id={$id}";
+		$query2 = mysqli_query($con, $sql2);
+		if(mysqli_num_rows($query2) != 0){
+			$row2 = mysqli_fetch_array($query2);
+			$_SESSION['cart'][$row2['id']] = array("quantity" => 1, "price" => $row2['price']);
+		}else{
+			$message = "This prodct is invalid";
+		}
+	}
+}
+if(isset($_GET['action']) && $_GET['action'] == "empty"){
+	$_SESSION['cart'] = null;
+}
+?>
+<h2 class="message"> <?php if(isset($message)){echo $message;}?></h2>
+<h1>Product Page</h1>
+<table>
+	<tr>
+		<th>Name</th>
+		<th>Description</th>
+		<th>Price</th>
+		<th>Action</th>
+	</tr>
+	<?php
+	$sql = "SELECT * FROM products ORDER BY name ASC";
+	$query = mysqli_query($con, $sql);
+	while($row = mysqli_fetch_assoc($query)){
+		?>
+		<tr>
+			<td><?php echo $row['name']; ?></td>
+			<td><?php echo $row['description']; ?></td>
+			<td><?php echo $row['price'] . " SEK"; ?></td>
+			<td><a href="products2.php?&action=add&id=<?php echo $row['id']; ?>">Add to Cart</a></td>
+		</tr>
+		<?php
+	}
+	?>
+</table>
+<td><a href="products2.php?&action=empty">Empty cart</a></td>
 
-<?php include 'header.php'; ?>
-
-<body>
-	
-	<!-- left side -->
-	<?php include 'left_side.php'; ?>
-
-		<!-- left section -->
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 white-bg right-container">
-			<div class="tm-right-inner-container">
-				<h1 class="templatemo-header">Our Products</h1>				
-				<article class="templatemo-item">
-					<h2>New Product</h2>
-					<img src="images/gutman-island.jpg" alt="Gutman Island" class="img-thumbnail">
-					<p>Black White is <a rel="nofollow" href="http://www.templatemo.com/page/1" title="free HTML5 template">free HTML5 template</a> by <span class="blue">template</span><span class="green">mo</span>. Nullam malesuada lacinia rhoncus. Nunc ut porta elit. Cras a ex enim. Etiam blandit sem vel lacinia interdum. Curabitur ut erat sed dui pulvinar tincidunt. Mauris vulputate dolor nibh, vel sodales urna eleifend id.</p>
-					<ul>
-						<li><i class="fa fa-check fa-medium"></i> Credit goes to <a rel="nofollow" href="http://unsplash.com">Unsplash</a> for images.</li>
-						<li><i class="fa fa-check fa-medium"></i> Cras bibendum porta purus feugiat scelerisque.</li>
-						<li><i class="fa fa-check fa-medium"></i> Vestibulum varius nibh ut diam commodo, sed volutpat libero pretium.</li>
-					</ul>															
-					<a href="#" class="btn btn-warning">Buy</a>			
-				</article><hr>
-				<article class="templatemo-item">
-					<h2>Second Product</h2>
-					<img src="images/japan.jpg" alt="Japan" class="img-thumbnail">
-					<p>
-						Maecenas in arcu eget velit lobortis dictum. Donec venenatis, justo quis efficitur pharetra, ligula ante pellentesque purus, sit amet tincidunt mauris elit non est. Etiam auctor, dui ut vehicula convallis, leo purus gravida massa, sed aliquet dui tellus rhoncus magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					</p>
-					<a href="#" class="btn btn-warning">Buy</a>				
-				</article><hr>
-				<article class="templatemo-item">
-					<h2>Product Three</h2>
-					<img src="images/fujifilm.jpg" alt="Fujifilm" class="img-thumbnail">
-					<p>
-						Etiam aliquet risus purus, at bibendum libero tempus sit amet. Pellentesque imperdiet, erat in finibus volutpat, nisi dui mattis erat, eu elementum libero mauris lobortis odio. Ut at purus mollis, rutrum augue quis, sollicitudin massa. Integer neque augue, lobortis at leo at, blandit euismod nunc.
-					</p>
-					<a href="#" class="btn btn-warning">Buy</a>				
-				</article>
-				<footer>
-					<p class="col-lg-6 col-md-6 col-sm-12 col-xs-12 templatemo-copyright">Copyright &copy; 2084 Your Company Name <!-- Credit: www.templatemo.com --></p>
-					<p class="col-lg-6 col-md-6 col-sm-12 col-xs-12 templatemo-social">
-						<a href="#"><i class="fa fa-facebook fa-medium"></i></a>
-						<a href="#"><i class="fa fa-twitter fa-medium"></i></a>
-						<a href="#"><i class="fa fa-google-plus fa-medium"></i></a>
-						<a href="#"><i class="fa fa-youtube fa-medium"></i></a>
-						<a href="#"><i class="fa fa-linkedin fa-medium"></i></a>
-					</p>
-				</footer>
-			</div>	
-		</div> <!-- right section -->
-	</div>	
-</body>
-</html>
+<h1>cart</h1>
+<?php
+if(isset($_SESSION['cart'])){
+	$sql = "SELECT * FROM products WHERE id IN (";
+		foreach($_SESSION['cart'] as $id => $value){
+			$sql .= $id . ",";
+		}
+		$sql = substr($sql,0,-1) . ") ORDER BY id ASC";
+$query = mysqli_query($con, $sql);
+while($row = mysqli_fetch_array($query)){
+	?> 
+	<p><?php echo $row['name']; ?> <?php echo " x" . $_SESSION['cart'][$row['id']]['quantity'];?></p>
+	<?php
+	$_SESSION['total'] = $_SESSION['total'] + $row['price'] * $_SESSION['cart'][$row['id']]['quantity'];
+}
+}else{
+	echo "<p>Your cart is empty. <br />Pleae add some products</p>";
+}
+echo $_SESSION['total'];
+echo "<a href='checkout.php'>Go to checkout</a>";
+?>
