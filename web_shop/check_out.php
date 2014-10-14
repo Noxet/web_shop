a67032d7cbbb0d36da03b52894a09c3a82bcc910
Noxet <nooxet@gmail.com>
2014-10-14 13:28:24 +0200

    added check_out.php and an image of cartman ofc

---
+++ web_shop/check_out.php
@@ -0,0 +1,26 @@
+<h1>Checkout</h1>
+<?php
+require_once "connection.php";
+$sql = "SELECT * FROM products ORDER BY name ASC";
+$query = mysqli_query($con, $sql);
+if(isset($_SESSION['cart'])){
+	$sql = "SELECT * FROM products WHERE id IN (";
+		foreach($_SESSION['cart'] as $id => $value){
+			$sql .= $id . ",";
+		}
+		$sql = substr($sql,0,-1) . ") ORDER BY id ASC";
+$query = mysqli_query($con, $sql);
+while($row = mysqli_fetch_array($query)){
+	?> 
+	<p><?php echo $row['name']; ?> <?php echo " x" . $_SESSION['cart'][$row['id']]['quantity'];?></p>
+	<?php
+}
+}else{
+	echo "<p>Your cart is empty. <br />Pleae add some products</p>";
+}
+echo "Total: " . $_SESSION['total'];
+?>
+<h2></h2>
+<td><a href="checkout.php">Buy with PayPal</a></td>
+<h3></h3>
+<td><a href="products.php">Back to shopping</a></td>
